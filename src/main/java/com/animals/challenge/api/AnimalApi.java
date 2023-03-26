1f0239eb0350e6193739fc12fd92b5366fdabffa
Wilker Machado <wilkerjmachado@gmail.com>
2023-03-26 10:53:47 -0300

    update README.md

--- src/main/java/com/animals/challenge/api/AnimalApi.java
+++ src/main/java/com/animals/challenge/api/AnimalApi.java
@@ -8,14 +8,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Getter
 @AllArgsConstructor
-@Controller("animal")
+@RestController
+@RequestMapping("animal")
 public class AnimalApi extends BaseApi<Animal, AnimalDto, AnimalService> {
 
     private AnimalService service;
